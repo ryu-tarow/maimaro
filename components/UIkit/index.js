a3da15edd5a365ba8340b404acc88dd5b836cd72
ryu-tarow <tarotarotarow@gmail.com>
2021-03-14 14:21:31 +0900

    fixed TitleBar

--- components/UIkit/index.js
+++ components/UIkit/index.js
@@ -1,7 +1,7 @@
 export { ShareButton } from "components/UIkit/ShareButton";
 export { SNSButton } from "components/UIkit/SNSButton";
 export { PhotoCard } from "components/UIkit/PhotoCard";
-export { TitleBar } from "components/UIkit/Titlebar";
+export { TitleBar } from "components/UIkit/TitleBar.tsx";
 export { ProfileBar } from "components/UIkit/ProfileBar";
 export { Photo } from "components/UIkit/Photo";
 export { PhotoCategory } from "components/UIkit/PhotoCategory";
